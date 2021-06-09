1008812fc4ce9753e201244c1dac369bc6b3527b
Jon Hallam <jon.hallam@ManagedDesktop.com>
2021-06-09 11:13:54 +0100

    try catch added, message if no param

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using Newtonsoft.Json;
 
 namespace Base64Files
@@ -10,43 +11,65 @@ namespace Base64Files
     {
         static void Main(string[] args)
         {
-            var path = args[0];
-            // var path = "/Users/jonhallam/Downloads";
+            try
+            {
+                var path = args[0];
+                //var path = "/Users/jonhallam/Downloads";
             
-            // var path = "/Users/jonhallam/Downloads/0000-004   34.xml";
-            var isDirectory = IsDirectory(path);
-            var files = Array.Empty<string>();
-            string json;
-            if (isDirectory) 
-            {
-                files = Directory.GetFiles(path);
-                List<Base64File> lb64F = new List<Base64File>();
-                foreach (var file in files)
+                //var path = "/Users/jonhallam/Downloads/0000-004   34.xml";
+            
+            
+            
+                var isDirectory = IsDirectory(path);
+                var files = Array.Empty<string>();
+                string json;
+                if (isDirectory) 
                 {
-                    lb64F.Add(GetFileAndContents(file));
+                    files = Directory.GetFiles(path);
+                    List<Base64File> lb64F = new List<Base64File>();
+                    foreach (var file in files)
+                    {
+                        lb64F.Add(GetFileAndContents(file));
+                    }
+                    json = JsonConvert.SerializeObject(lb64F, Formatting.Indented);
                 }
-                json = JsonConvert.SerializeObject(lb64F, Formatting.Indented);
+                else
+                {
+                    files = new string[] {path};
+                    var singleFile = GetFileAndContents(files.ElementAt((0)));
+                    json = JsonConvert.SerializeObject(singleFile, Formatting.Indented);
+                    
+                }
+               
+                Console.WriteLine(json);
             }
-            else
+            catch (Exception e)
             {
-                files = new string[] {path};
-                var singleFile = GetFileAndContents(files.ElementAt((0)));
-                json = JsonConvert.SerializeObject(singleFile, Formatting.Indented);
-                
+                Console.WriteLine("either a full path or a fill file location needs to be added to the end of the commandline tool");
+                throw;
             }
-           
-            Console.WriteLine(json);
         }
 
         private static Base64File GetFileAndContents(string file)
         {
             if (file.Contains(".DS_Store") || file.Contains(".localized")) return null;
+            string filename;
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                filename = file.Split("\"").Last();
+            }
+            else
+            {
+                filename = file.Split("/").Last();
+            }
+
             var returnData = new Base64File
             {
                 Contents = Convert.ToBase64String(File.ReadAllBytes(file)),
-                FileName = file.Split('/').Last()
-
+                FileName = filename
             };
+
+            
             return returnData;
 
         }
